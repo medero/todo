7b4e0cbe045f41aaaef0095da87d0aab61db6c7b
Meder Omuraliev <itmpls@gmail.com>
2015-11-18 02:21:16 -0500

    .

--- todo.js
+++ todo.js
@@ -1,28 +1,60 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
 
     Template.body.helpers({
-        tasks: [
-        { text: 'This is task 1' },
-        { text: 'This is task 2' },
-        { text: 'This is task 3' }
-        ]
-    })
+        tasks: function () {
+            return Tasks.find({}, {sort: {createdAt:-1}});
+        }
+    });
 
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+    Template.body.events({
+        "submit .new-task": function (event) {
+            // Prevent default browser form submit
+            event.preventDefault();
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+            // Get value from form element
+            var text = event.target.text.value;
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+            // Insert a task into the collection
+            Tasks.insert({
+                text: text,
+                createdAt: new Date() // current time
+            });
+
+            // Clear form
+            event.target.text.value = "";
+        }
+    });
+
+    // counter starts at 0
+    Session.setDefault('counter', 0);
+
+    Template.hello.helpers({
+        counter: function () {
+            return Session.get('counter');
+        }
+    });
+
+    Template.hello.events({
+        'click button': function () {
+            // increment the counter when button is clicked
+            Session.set('counter', Session.get('counter') + 1);
+        }
+    });
+
+    Template.task.events({
+        'click .toggle-checked': function() {
+            // Set the checked property to the opposite of its current value
+
+            Tasks.update(this._id, {
+                $set: {checked: !this.checked}
+            });
+         }, 
+         'click .delete':function() {
+             Tasks.remove(this._id);
+         }
+    });
 }
 
 if (Meteor.isServer) {
